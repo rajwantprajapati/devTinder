da63ef7269275fb5b2fea54c8a73fe965d9f0b67
Rajwant Prajapati <rajwantprajapati5791@gmail.com>
2025-12-28 15:41:43 +0530

    feat: Implement /user api to get user by id and /feed api to get all the users from DB

--- src/app.mjs
+++ src/app.mjs
@@ -4,21 +4,21 @@ import User from "./models/user.mjs";
 
 const app = express();
 
+// req body parser middleware
+app.use(express.json());
+
 try {
   await connectDB();
 
   console.log("DB Connection Success!!");
 
+  /**
+   * Signup API
+   * POST /signup - Create new user and save into database
+   */
   app.post("/signup", async (req, res) => {
-    const userData = {
-      firstName: "Rajwant",
-      lastName: "Prajapati",
-      email: "rajwant@gmail.com",
-      age: "34",
-      geneder: "M",
-    };
-
-    const user = new User(userData);
+    // Create new instance of user model with req body
+    const user = new User(req.body);
 
     try {
       await user.save();
@@ -28,6 +28,52 @@ try {
       console.log("signup error: ", eror);
 
       res.status(400).send("error while signup!!");
+    }
+  });
+
+  /**
+   * Find User by email id
+   * GET /user - get user by email id
+   */
+  app.get("/user", async (req, res) => {
+    const userEmailId = req.body.emailId;
+
+    try {
+      const user = await User.findOne({ emailId: userEmailId });
+
+      if (!user) {
+        return res.status(404).send({ error: { message: "User not found!" } });
+      }
+
+      res.status(200).send(user);
+    } catch (error) {
+      console.log("error in fetching user by email: ", error);
+
+      res.status(400).send({
+        error: { message: "Something went wrong in fetching user by email id" },
+      });
+    }
+  });
+
+  /**
+   * Feed API
+   * GET /feed - get all the users from database
+   */
+  app.get("/feed", async (req, res) => {
+    try {
+      const users = await User.find();
+
+      if (users?.length === 0) {
+        return res.status(404).send("No user found!");
+      }
+
+      res.status(200).send(users);
+    } catch (error) {
+      console.log("error in fetching users ", error);
+
+      res.status(400).send({
+        error: { message: "Something went wrong in fetching users" },
+      });
     }
   });
 
