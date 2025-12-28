cf87fba7cfe44339fb1d69abfc79a9ce7be31177
Rajwant Prajapati <rajwantprajapati5791@gmail.com>
2025-12-28 16:02:59 +0530

    fix: remove cred

--- src/config/database.mjs
+++ src/config/database.mjs
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://rajwant:rajwant@devtinder.28whu29.mongodb.net/devTinder",
+    "mongodb+srv://<username>:<password>@devtinder.28whu29.mongodb.net/devTinder",
   );
 };
